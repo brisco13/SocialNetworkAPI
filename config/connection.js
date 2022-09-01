b9aa8f9eea26d9f97d52c92189b49bdf51bb8133
Brisco Arechederra <brisco.arch@gmail.com>
2022-09-01 14:25:39 -0600

    connection.js fille complete.

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+// require mongoose
+const { connect, connection } = require("mongoose");
+
+// connection string to port defined in .env file or the local DB
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialNetwork";
+
+// call to connect to the connection string
+connect(connectiongString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// export this connection
+module.exports = connection;
